143a02b8a2c965c4267f7718c3fd47e3e898ed79
tharindu ariyarathna <tharinduariyarathna94@gmail.com>
2018-01-16 00:30:57 +0530

    Update and rename MethodOverloadingWrong.java to MethodOverloading.java

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -12,3 +12,25 @@ public class Test{
     }
 
 }
+//Correct code...
+public class Test{
+
+    public static void method1(int x){
+
+        System.out.println("method1");
+
+    }
+
+    public static void method1(){
+
+        System.out.println("method2");
+
+    }
+
+     public static void main(String[] args) {
+
+        method1(5);
+
+    }
+
+}
